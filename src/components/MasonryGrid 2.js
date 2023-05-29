a93d503c3651c96c93ea10435853e67c25fd263b
Yusong Shi <jackshi.sys@gmail.com>
2023-05-29 15:26:49 -0400

    Plan your day

--- src/components/MasonryGrid 2.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const MasonryGrid = ({ children }) => {
-  return <div className="gap-2 columns-2 lg:columns-3">{children}</div>;
-};
-
-export default MasonryGrid;
--- src/pages/planyourday.js
+++ src/pages/planyourday.js
@@ -2,6 +2,8 @@ import React from "react";
 import Layout from "../components/Layout";
 import Tiltlebg from "../assets/Cascadep-Spa-title.png";
 import PlanDayHero from "../assets/Cascade_Spa_Images-5.jpg";
+import Plansection from "../assets/Cascade Spa Images-28.jpg";
+import Carousel from "../components/Carousal";
 
 const PlanYourDay = () => {
   return (
@@ -22,6 +24,42 @@ const PlanYourDay = () => {
             <span className="text-white text-[1.8rem] lg:text-[2rem] font-titleBig">
               PLAN YOUR DAY
             </span>
+          </div>
+        </div>
+      </section>
+
+      <section className="max-w-4xl mx-auto text-center">
+        <div className="px-4 md:px-8 lg:px-0 my-6 lg:my-12">
+          <p>
+            Here at Cascade Spa, we invite you to indulge in the ultimate
+            upscale oasis in beautiful Hillcrest. Breathe in the tranquil aromas
+            the moment you walk into this luxurious spa and secret hide-away.
+          </p>
+        </div>
+      </section>
+
+      <section className="max-w-4xl mx-auto my-12 lg:my-32">
+        <div className="px-4 md:px-8 lg:px-0 my-6 lg:my-12 flex flex-col-reverse lg:flex-row gap-8">
+          <div className="flex flex-col justify-center w-full md:w-1/3 text-lg font-extralight gap-6">
+            <div>
+              <h2 className="uppercase font-titleBig text-lg">
+                Group, Gathering, & Parties.
+              </h2>
+              <span className="uppercase font-title text-sm">
+                (CALL FOR MORE INFORMATION)
+              </span>
+            </div>
+            <p className="text-sm font-title font-extralight">
+              It’s party time! Whether its friends gathering, girls night out,
+              bachelor or bachelorette parties, birthday parties or even a
+              company event/retreat. Let us be your guide and the place to go.
+              We offer the best spa parties in the area! Your party could only
+              be the best experience possible. Questions? Please contact us by
+              calling 619-542-9778
+            </p>
+          </div>
+          <div className="w-full md:w-2/3 h-auto">
+            <Carousel />
           </div>
         </div>
       </section>
